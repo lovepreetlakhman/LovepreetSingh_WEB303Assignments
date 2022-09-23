53b470004025fd4666df3d92e12eba5057da9837
Lovepreet Singh <105748512+lovepreetlakhman@users.noreply.github.com>
2022-09-22 22:45:25 -0400

    Add files via upload

---
+++ WEB303As01/script.js
@@ -0,0 +1,18 @@
+/*
+	WEB 303 Assignment 1 - jQuery
+	Lovepreet Singh
+*/
+
+$("document").ready(() => {
+
+	const yearlySalary = $("#yearly-salary");
+	const techPercent = $("#percent");
+	const techTotal = $("#amount");
+
+	function calculateExpense(){
+		let amount = yearlySalary.val() * techPercent.val() / 100;
+		techTotal.text(`$${amount.toFixed(2)}`);
+	}
+	yearlySalary.keyup(calculateExpense);
+	techPercent.keyup(calculateExpense);
+});
